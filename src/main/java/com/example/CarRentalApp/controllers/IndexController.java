6e66ea049fb1912c5a70e323afdb631c4d3e78c8
Mirza Subasic <mirza.suba@gmail.com>
2023-09-18 17:15:48 +0200

    Implement car service

--- src/main/java/com/example/CarRentalApp/controllers/IndexController.java
+++ src/main/java/com/example/CarRentalApp/controllers/IndexController.java
@@ -1,7 +1,5 @@
 package com.example.CarRentalApp.controllers;
 
-import com.example.CarRentalApp.model.Car;
-import com.example.CarRentalApp.repository.CarRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,10 +8,6 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
     public String index(){
-
-        Car car = CarRepository.findById(1L).orElse(null);
-
-        model.addAttribute("car", car);
 
         return "index";
     }
---
+++ src/main/java/com/example/CarRentalApp/service/implementation/CarServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.CarRentalApp.service.implementation;
+
+import com.example.CarRentalApp.model.Car;
+import com.example.CarRentalApp.repository.CarRepository;
+import com.example.CarRentalApp.service.CarService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@Service
+public class CarServiceImpl implements CarService {
+
+    private final CarRepository carRepository;
+
+    @Override
+    public Set<Car> findAll() {
+        Set<Car> car = new HashSet<>();
+        carRepository.findAll().forEach(car::add);
+        return car;
+    }
+
+    @Override
+    public Car findById(Long aLong) {
+        return carRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Car save(Car object) {
+        return carRepository.save(object);
+    }
+
+    @Override
+    public void delete(Car object) {
+        carRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        carRepository.deleteById(aLong);
+    }
+}
