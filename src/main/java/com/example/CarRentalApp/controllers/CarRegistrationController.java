cb1ac31c0094e01600f79b1391eecb8d7958264b
Mirza Subasic <mirza.suba@gmail.com>
2023-09-28 15:25:02 +0200

    Show car registration info on separate page. closes #16

---
+++ src/main/java/com/example/CarRentalApp/controllers/CarRegistrationController.java
@@ -0,0 +1,23 @@
+package com.example.CarRentalApp.controllers;
+
+import com.example.CarRentalApp.service.CarRegistrationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("registration/")
+@AllArgsConstructor
+@Controller
+public class CarRegistrationController {
+
+    private final CarRegistrationService carRegistrationService;
+
+
+    @GetMapping("")
+    public String listCarRegistrations(Model model) {
+        model.addAttribute("carRegistrations", carRegistrationService.findAll());
+        return "cars/carRegistrationDetails";
+    }
+}
